dbff5ac21c4193000b5fed0ea97f04a0ff47dc15
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2025-10-09 09:08:55 +0200

    chore: add Jest configuration and update Babel presets for testing

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,29 @@ module.exports = {
   presets: [
     ['module:react-native-builder-bob/babel-preset', { modules: 'commonjs' }],
   ],
+  env: {
+    test: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-react',
+        [
+          '@babel/preset-typescript',
+          { allowDeclareFields: true, isTSX: true, allExtensions: true },
+        ],
+      ],
+    },
+  },
+  overrides: [
+    {
+      test: /node_modules/,
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-react',
+        [
+          '@babel/preset-typescript',
+          { allowDeclareFields: true, isTSX: true, allExtensions: true },
+        ],
+      ],
+    },
+  ],
 };
